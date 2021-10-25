cd0aa57b53f3ce3316dcb8941c5f403d1be64928
LENOVO <deyzaaditya9@gmail.com>
2021-10-25 15:42:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tika22/MenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.tika22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
